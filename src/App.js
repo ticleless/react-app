198fe14deb00ff1ba07a0ff9a850e23f58ebe300
ticleless <kyy20802@gmail.com>
2023-07-14 19:26:59 +0900

    my first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>React는 처음이야 끼요잇!</h1>
+    </header>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <p>너무나 설레자너 헤헷 히힛 호홍</p>
+    </article>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Header></Header>
+      <Article></Article>
+      <Article></Article>
     </div>
   );
+    
 }
 
 export default App;
